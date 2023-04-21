f735d4bbd0bc26cdb32a4483603916341857a5f1
Michael Friendly <friendly@yorku.ca>
2023-04-20 21:03:46 -0400

    finish draft of augment.nested

--- examples/augment.nested.R
+++ examples/augment.nested.R
@@ -2,6 +2,47 @@
 #' title: try broom::augment
 #' ---
 #'
+
+# quick try at an augment function
+augment.nested <- function(x,
+                           data = NULL,
+                           newdata = NULL,
+                           type.predict = c("link", "response"),
+                           type.residuals = c("deviance", "pearson"),
+                           se_fit = TRUE,    # it is FALSE in augment.glm()
+                           ...)
+{
+  if (is.null(data))
+    data <- model.frame(x$models[[1L]])
+  if (missing(newdata))
+    newdata <- x$models[[1L]]$data
+
+  type.predict <- match.arg(type.predict)
+  if (type.predict != "link") stop("type.predict = ", type.predict, " is not implemented.")
+
+  nms <- names(x$models)
+  result <- lapply(x$models, broom::augment,
+                   data=data,
+                   newdata=newdata,
+                   type.predict = type.predict,
+                   type.residuals = type.residuals,
+                   se_fit = se_fit,
+                   ...
+  )
+
+  cls <- class(result[[1L]])
+  for (i in seq_along(x$models)) {
+    result[[i]] <- cbind(response = nms[i], result[[i]])
+  }
+  result <- dplyr::bind_rows(result) |>
+    select(-..y)
+  class(result) <- cls
+  result
+
+}
+
+
+
 library(nestedLogit)
 library(dplyr)
 library(broom)
@@ -20,44 +61,16 @@ broom::augment(wlf.nested$models[[1]], type.predict = "response") |> head()
 broom::augment(wlf.nested$models[[1]], type.predict = "response", se_fit = TRUE) |> head()
 
 
+
 wlf.augmented <- lapply(wlf.nested$models, broom::augment)
 names(wlf.augmented)
 
+# try our function
+wlf.aug <- augment(wlf.nested)
 
-# quick try at an augment function
-nested.augment <- function(x,
-                           data = NULL,
-                           newdata = NULL,
-                           type.predict = c("link", "response"),
-                           type.residuals = c("deviance", "pearson"),
-                           se_fit = TRUE,    # it is FALSE in augment.glm()
-                           ...)
-{
-  if (is.null(data))
-    data <- model.frame(x$models[[1L]])
-  if (missing(newdata))
-    newdata <- x$models[[1L]]$data
+names(wlf.aug)
+# why don't we get the other variables computed by broom::augment.glm?
+names(wlf.augmented[[1]])
 
-  type.predict <- match.arg(type.predict)
-  if (type.predict != "link") stop("type.predict = ", type.predict, " is not implemented.")
 
-  nms <- names(object$models)
-  result <- lapply(x, augment,
-                   data=data,
-                   newdata=newdata,
-                   type.predict = type.predict,
-                   type.residuals = type.residuals,
-                   se_fit = se_fit,
-                   ...
-                   )
-
-  cls <- class(result[[1L]])
-  for (i in seq_along(x)) {
-    result[[i]] <- cbind(response = nms[i], result[[i]])
-  }
-  result <- dplyr::bind_rows(result)
-  class(result) <- cls
-  result
-
-}
 
