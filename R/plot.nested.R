3a9b44110632450ad5edabb9541f3419525d8c27
John Fox <jfox@mcmaster.ca>
2023-04-29 10:41:19 -0400

    improvements to plot.nestedLogit()

--- R/plot.nested.R
+++ R/plot.nested.R
@@ -8,9 +8,13 @@
 #' on the horizontal axis and other explanatory variables fixed to particular values.
 #' @seealso \code{\link{nestedLogit}}, \code{\link[graphics]{matplot}}
 #' @param x an object of \code{"nestedLogit"} produced by \code{\link{nestedLogit}}.
-#' @param x.var quoted name of the variable to appear on the x-axis.
+#' @param x.var quoted name of the variable to appear on the x-axis; if omitted, the first
+#'        predictor in the model is used.
 #' @param others a named list of values for the other variables in the model,
-#'        that is, other than \code{x.var}.
+#'        that is, other than \code{x.var}; if any other predictor is omitted, it is set
+#'        to an arbitrary value---the mean for a numeric predictor or the first level or
+#'        value of a factor, character, or logical predictor; only one value may be
+#'        specified for each variable in \code{others}.
 #' @param n.x.values the number of evenly spaced values of \code{x.var} at which
 #'        to evaluate fitted probabilities to be plotted (default \code{100}).
 #' @param xlab label for the x-axis (defaults to the value of \code{x.var}).
@@ -36,6 +40,7 @@
 #'                  logits(work=dichotomy("not.work", c("parttime", "fulltime")),
 #'                         full=dichotomy("parttime", "fulltime")),
 #'                         data=Womenlf)
+#' plot(m)
 #' op <- par(mfcol=c(1, 2), mar=c(4, 4, 3, 1) + 0.1)
 #' plot(m, "hincome", list(children="absent"),
 #'      xlab="Husband's Income", legend=FALSE)
@@ -47,19 +52,25 @@
 #' @importFrom stats formula
 #' @rdname plot.nestedLogit
 #' @export
-plot.nestedLogit <- function(x, x.var, others, n.x.values=100,
-                        xlab=x.var, ylab="Fitted Probability",
-                        main, cex.main=1, font.main=1,
-                        pch=1:length(response.levels),
-                        lwd=3, lty=1:length(response.levels),
-                        col=palette()[1:length(response.levels)],
-                        legend=TRUE, legend.inset=0.01,
-                        legend.location="topleft", ...){
+plot.nestedLogit <- function(x, x.var, others, n.x.values=100L,
+                             xlab=x.var, ylab="Fitted Probability",
+                             main, cex.main=1, font.main=1L,
+                             pch=1L:length(response.levels),
+                             lwd=3, lty=1L:length(response.levels),
+                             col=palette()[1L:length(response.levels)],
+                             legend=TRUE, legend.inset=0.01,
+                             legend.location="topleft", ...){
   data <- x$data
-  vars <- all.vars(formula(x)[-2])
+  vars <- all.vars(formula(x)[-2L])
   response <- setdiff(all.vars(formula(x)), vars)
+  if (missing(x.var)) {
+    x.var <- setdiff(vars, response)[1L]
+    message("Note: ", x.var, " will be used for the horizontal axis")
+  }
   if (!(x.var %in% vars)) stop (x.var, " is not in the model")
   if (!missing(others)){
+    if (any(sapply(others, length) > 1)) 
+      stop("more than one value specified for one or more variables in others")
     other.x <- names(others)
     check <- other.x %in% vars
     if (any(!check)) stop("not in the model: ", paste(other.x[!check], collapse=", "))
@@ -67,10 +78,31 @@ plot.nestedLogit <- function(x, x.var, others, n.x.values=100,
   } else {
     values <- list()
   }
+  other.xs <- setdiff(vars, c(response, x.var))
+  missing.xs <- !(other.xs %in% names(values))
+  if (any(missing.xs)){
+    missing.xs <- other.xs[missing.xs]
+    for (missing.x in missing.xs){
+      xx <- x$data[, missing.x]
+      if (is.numeric(xx)){
+        values[[missing.x]] <- signif(mean(xx, na.rm=TRUE))
+        message("Note: missing predictor ", missing.x, " set to its mean, ", 
+                values[[missing.x]])
+      } else if (is.factor(xx)){
+        values[[missing.x]] <- levels(xx)[1L]
+        message("Note: missing predictor ", missing.x, " set to its first level, '", 
+                values[[missing.x]], "'")
+      } else {
+        values[[missing.x]] <-  (sort(unique(xx)))[1L]
+        message("Note: missing predictor ", missing.x, " set to its first value, '", 
+                values[[missing.x]], "'")
+      }
+    }
+  }
   if (is.numeric(data[, x.var])){
     numeric.x <- TRUE
     range.x <- range(data[, x.var], na.rm=TRUE)
-    values[[x.var]] <- seq(range.x[1], range.x[2], length=n.x.values)
+    values[[x.var]] <- seq(range.x[1L], range.x[2L], length=n.x.values)
   } else if (is.factor(data[, x.var])){
     numeric.x <- FALSE
     values[[x.var]] <- levels(data[, x.var])
@@ -83,28 +115,31 @@ plot.nestedLogit <- function(x, x.var, others, n.x.values=100,
   response.levels <- levels(data[[response]])
   if (numeric.x){
     matplot(new[, x.var], new[, response.levels], type="l", lwd=lwd,
-          col=col, xlab=xlab, ylab=ylab)
+            col=col, xlab=xlab, ylab=ylab)
     if (legend) legend(legend.location, legend=response.levels, lty=lty, lwd=lwd,
-           col=col, inset=legend.inset, xpd=TRUE)
+                       col=col, inset=legend.inset, xpd=TRUE)
   } else {
     n.x.levels <- nrow(new)
-    matplot(1:n.x.levels, new[, response.levels], type="b", lwd=lwd,
+    matplot(1L:n.x.levels, new[, response.levels], type="b", lwd=lwd,
             pch=pch, col=col, xlab=xlab, ylab=ylab, axes=FALSE)
     box()
-    axis(2)
-    axis(1, at=1:n.x.levels, labels=new[, x.var])
+    axis(2L)
+    axis(1L, at=1L:n.x.levels, labels=new[, x.var])
     if (legend) legend(legend.location, legend=response.levels, lty=lty, lwd=lwd,
-           col=col, pch=pch, inset=legend.inset, xpd=TRUE)
+                       col=col, pch=pch, inset=legend.inset, xpd=TRUE)
   }
   if (!missing(main)) {
     title(main, cex.main=cex.main, font.main=font.main)
   } else {
-    if (!missing(others)){
-      main <- paste(paste(other.x, "=", as.character(unlist(others))),
-                                   collapse=", ")
-      title(main, cex.main=cex.main, font.main=font.main)
+    if (length(values) != 0L){
+      which.main <- !(x.var == names(values))
+      if (any(which.main)){
+        main <- paste(paste(names(values[which.main]), "=", 
+                            as.character(unlist(values[which.main]))),
+                      collapse=", ")
+        title(main, cex.main=cex.main, font.main=font.main)
+      }
     }
   }
   return(invisible(NULL))
 }
-
