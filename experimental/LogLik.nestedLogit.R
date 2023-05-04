34763252c7cb4d954ca11f1cfb09475fc53cdf38
Michael Friendly <friendly@yorku.ca>
2023-05-04 11:10:45 -0400

    add example

--- experimental/LogLik.nestedLogit.R
+++ experimental/LogLik.nestedLogit.R
@@ -11,3 +11,22 @@ logLik.nestedLogit <- function(object, ...){
   class(result) <- "logLik"
   result
 }
+
+# Example
+if (FALSE){
+  data(Womenlf, package = "carData")
+
+  # compare different ways of specifying the dichotomies
+  m1 <- nestedLogit(partic ~ hincome + children,
+                    logits(work=dichotomy("not.work", working=c("parttime", "fulltime")),
+                           full=dichotomy("parttime", "fulltime")),
+                    data=Womenlf)
+
+  m2 <- nestedLogit(partic ~ hincome + children,
+                    logits(work=dichotomy(nonfulltime=c("not.work", "parttime"), "fulltime"),
+                           full=dichotomy("not.work", "parttime")),
+                    data=Womenlf)
+  logLik(m1)
+  logLik(m2)
+  AIC(m1, m2)
+}
