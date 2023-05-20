b2d4a91a4c327500e60275a61572f44a69d4460d
Michael Friendly <friendly@yorku.ca>
2023-05-19 22:05:40 -0400

    fixup predict-with-se-example

--- examples/predict-with-se-example.R
+++ examples/predict-with-se-example.R
@@ -12,12 +12,12 @@ comparisons <- logits(work=dichotomy("not.work", c("parttime", "fulltime")),
 wlf.nested <- nestedLogit(partic ~ hincome + children,
                           dichotomies = comparisons,
                           data=Womenlf)
-
-new <- expand.grid(hincome=seq(0, 45, length=10),
+# get predicted values for a grid
+new <- expand.grid(hincome=seq(0, 45, length=4),
                    children=c("absent", "present"))
 
 pred.nested <- predict(wlf.nested, new)
-pred.nested
+names(pred.nested)
 
 rowSums(pred.nested$p)
 
@@ -40,37 +40,68 @@ pred.nested.df <-lapply(pred.nested.df, cbind, new)
 
 # make a dataset for plotting
 
-p  <- pred.nested.df$p  |> relocate(hincome, children)
+prob  <- pred.nested.df$p  |> relocate(hincome, children)
 
-se <- pred.nested.df$se.p |> relocate(hincome, children)
+se.prob <- pred.nested.df$se.p |> relocate(hincome, children)
 resp.names <- colnames(p)
 
-p_long <- p |>
+p_long <- prob |>
   tidyr::pivot_longer(cols = not.work:fulltime,
                       names_to = "response",
                       values_to = "prob")
 
-se_long <- se |>
+se_long <- se.prob |>
   tidyr::pivot_longer(cols = not.work:fulltime,
                       names_to = "response",
-                      values_to = "se")
+                      values_to = "se.prob")
 
-plotdata <- cbind(p_long, se = se_long$se)
+plotprob <- cbind(p_long, se.prob = se_long$se.prob)
 
-gg <- ggplot(plotdata,
+ggplot(plotprob,
              aes(x=hincome, y=prob, color=response)) +
   geom_line(linewidth = 2) +
   geom_point(size = 1.5, shape = 16, color = "black") +
   scale_color_discrete() +
   labs(x="Husband's Income", y= "Probability") +
   facet_wrap(~ children, labeller = label_both) +
-  theme_bw(base_size = 14)
-
-gg + geom_ribbon(aes(ymin=prob - se,
-                     ymax=prob + se,
+  theme_bw(base_size = 14) +
+  geom_ribbon(aes(ymin=prob - se.prob,
+                     ymax=prob + se.prob,
                      fill = response), alpha = 0.3) +
-  geom_textline(aes(label = response), hjust = -0.05, vjust=-0.5, size=5) +
+  geom_textline(aes(label = response), hjust = -0.01, vjust=-0.5, size=5) +
   theme(legend.position = "none")
 
+# --------------------------------------------
+# do the same for logit
 
+logit  <- pred.nested.df$logit  |> relocate(hincome, children)
+
+se.logit <- pred.nested.df$se.logit |> relocate(hincome, children)
+resp.names <- colnames(p)
+
+p_long <- logit |>
+  tidyr::pivot_longer(cols = not.work:fulltime,
+                      names_to = "response",
+                      values_to = "logit")
+
+se_long <- se.logit |>
+  tidyr::pivot_longer(cols = not.work:fulltime,
+                      names_to = "response",
+                      values_to = "se.logit")
+
+plotlogit <- cbind(p_long, se.logit = se_long$se.logit)
+
+ggplot(plotlogit,
+       aes(x=hincome, y=logit, color=response)) +
+  geom_line(linewidth = 2) +
+  geom_point(size = 1.5, shape = 16, color = "black") +
+  scale_color_discrete() +
+  labs(x="Husband's Income", y= "Log Odds") +
+  facet_wrap(~ children, labeller = label_both) +
+  theme_bw(base_size = 14) +
+  geom_ribbon(aes(ymin=logit - se.logit,
+                  ymax=logit + se.logit,
+                  fill = response), alpha = 0.3) +
+  geom_textline(aes(label = response), hjust = -0.01, vjust=-0.5, size=5) +
+  theme(legend.position = "none")
 
