a9b252cc0ca755a50f103b0fc27e0f7b50a0115d
John Fox <jfox@mcmaster.ca>
2023-05-25 15:06:51 -0400

    add health insurance example to test-SEs.R

--- experimental/test-SEs.R
+++ experimental/test-SEs.R
@@ -97,3 +97,65 @@ se.p.d <- sqrt((p2*p3)^2*v1 + (p1*p3)^2*v2 + (p1*p2)^2*v3)
 
 all.equal(cbind(se.p.a, se.p.b, se.p.c, se.p.d), as.matrix(pred.gss$se.p), 
           check.attributes=FALSE)
+
+
+# ------------- health-insurance example --------
+
+lbinary <- logits(AB_CD = dichotomy(c("A", "B"), c("C", "D")),
+                  A_B   = dichotomy("A", "B"),
+                  C_D   = dichotomy("C", "D"))
+
+health.nested <- nestedLogit(product4 ~ age  + gender * household + position_level,
+                             dichotomies = lbinary, data = HealthInsurance)
+
+HealthInsurance$y1 <- with(HealthInsurance, ifelse(product4 %in% c("A", "B"),0, 1))
+HealthInsurance$y2 <- with(HealthInsurance, ifelse(product4 == "A", 0,
+                                                   ifelse(product4 == "B", 1, NA)))
+HealthInsurance$y3 <- with(HealthInsurance, ifelse(product4 == "C", 0,
+                                                   ifelse(product4 == "D", 1, NA)))
+xtabs(~ product4 + y1, data=HealthInsurance)
+xtabs(~ product4 + y2, data=HealthInsurance)
+xtabs(~ product4 + y3, data=HealthInsurance)
+
+m1 <- glm(y1 ~  age  + gender * household + position_level, 
+          data=HealthInsurance, family=binomial)
+m2 <- glm(y2 ~  age  + gender * household + position_level, 
+          data=HealthInsurance, family=binomial)
+m3 <- glm(y3 ~  age  + gender * household + position_level, 
+          data=HealthInsurance, family=binomial)
+
+new <- expand.grid(age=seq(20, 70, length=3), 
+                   gender = c("Female", "Male"),
+                   household = mean(HealthInsurance$household),
+                   position_level = mean(HealthInsurance$position_level)
+)
+
+pr1 <- predict(m1, newdata=new, type="response", se.fit=TRUE)
+p1 <- pr1$fit
+v1 <- (pr1$se.fit)^2
+
+pr2 <- predict(m2, newdata=new, type="response", se.fit=TRUE)
+p2 <- pr2$fit
+v2 <- (pr2$se.fit)^2
+
+pr3 <- predict(m3, newdata=new, type="response", se.fit=TRUE)
+p3 <- pr3$fit
+v3 <- (pr3$se.fit)^2
+
+pred.health <- predict(health.nested, newdata=new)
+
+p.a <- (1 - p1)*(1 - p2)
+p.b <- (1 - p1)*p2
+p.c <- p1*(1 - p3)
+p.d <- p1*p3
+
+all.equal(cbind(p.a, p.b, p.c, p.d), as.matrix(pred.health$p), 
+          check.attributes=FALSE)
+
+se.p.a <- sqrt((1 - p2)^2*v1 + (1 - p1)^2*v2)
+se.p.b <- sqrt(p2^2*v1 + (1 - p1)^2*v2)
+se.p.c <- sqrt((1 - p3)^2*v1 + p1^2*v3)
+se.p.d <- sqrt(p3^2*v1 + p1^2*v3)
+
+all.equal(cbind(se.p.a, se.p.b, se.p.c, se.p.d), as.matrix(pred.health$se.p), 
+          check.attributes=FALSE)
