72b2ab2b366b5621b02161d379e12b42602f4e99
Michael Friendly <friendly@yorku.ca>
2023-05-28 12:49:13 -0400

    add ploting-ggplot vignette

--- examples/wlf-ggplot.R
+++ examples/wlf-ggplot.R
@@ -31,20 +31,30 @@ ggplot(plotdata,
   geom_ribbon(aes(ymin=p - se.p,
                   ymax=p + se.p,
                   fill = response), alpha = 0.3) +
-  geom_textline(aes(label = response), hjust = -0.01, vjust=-0.5, size=5) +
+  geom_textline(aes(label = response),
+                hjust = -0.01, vjust=-0.5, size=5) +
   theme(legend.position = "none")
 
+# Plot the corresponding logits
 ggplot(plotdata,
        aes(x=hincome, y=logit, color=response)) +
   geom_line(linewidth = 2) +
   geom_point(size = 1.5, shape = 16, color = "black") +
   scale_color_discrete() +
-  labs(x="Husband's Income", y= "Probability") +
+  labs(x="Husband's Income", y= "Log Odds") +
   facet_wrap(~ children, labeller = label_both) +
   theme_bw(base_size = 14) +
   geom_ribbon(aes(ymin=logit - se.logit,
                   ymax=logit + se.logit,
                   fill = response), alpha = 0.3) +
-  geom_textline(aes(label = response), hjust = -0.01, vjust=-0.5, size=5) +
+  geom_textline(aes(label = response),
+                hjust = -0.01, vjust=-0.5, size=5) +
   theme(legend.position = "none")
+
+
+# Predicted values for the dichotomies
+
+pred.dichot <- predict(wlf.nested, newdata = new,
+                       model = "dichotomies", se.fit = TRUE)
+
 
