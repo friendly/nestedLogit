3bc7ba0fa0b5610745a184c2203ae93d70b84f44
Michael Friendly <friendly@yorku.ca>
2023-05-30 11:56:47 -0400

    link se vignette in main one

--- examples/parameters-test.R
+++ examples/parameters-test.R
@@ -4,6 +4,7 @@
 #'
 library(nestedLogit)
 library(parameters)
+library(performance)
 data(Womenlf, package="carData")
 comparisons <- logits(work=dichotomy("not.work", c("parttime", "fulltime")),
                       full=dichotomy("parttime", "fulltime"))
@@ -16,4 +17,5 @@ wlf.nested <- nestedLogit(partic ~ hincome + children,
 parameters(wlf.nested, exponentiate = TRUE)
 
 # see::plot(parm) -- doesn't do anything useful
-
+# check <- performance::check_normality(wlf.nested)
+# Checking normality of residuals is only appropriate for linear models.
